4e0003f29a0d2e9190b483193eb8bdc027457fa3
Natheus <natheusmunes@gmail.com>
2022-08-20 14:40:17 -0300

    add intro game

--- config.py
+++ config.py
@@ -14,6 +14,7 @@ ENEMY_SPEED = 2
 RED = (255, 0, 0)
 BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
 
 tilemap = [
     'BBBBBBBBBBBBBBBBBBBB',
--- main.py
+++ main.py
@@ -9,10 +9,12 @@ class Game:
         self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.running = True
+        self.font = pygame.font.Font('arial.ttf', 32)
         
         self.character_spritesheet = Spritesheet('img/character.png')
         self.terrain_spritesheet = Spritesheet('img/terrain.png')
         self.enemy_spritesheet = Spritesheet('img/enemy.png')
+        self.intro_background = pygame.image.load('img/introbackground.png')
 
     def createTilemap(self):
         for i, row in enumerate(tilemap):
@@ -66,7 +68,29 @@ class Game:
         pass
 
     def intro_screen(self):
-        pass
+        intro = True
+        
+        title = self.font.render('Titi game', True, BLACK)
+        title_rect = title.get_rect(x=10, y=10)
+        
+        play_button = Button(10, 50, 100, 50, WHITE, BLACK, 'Play', 32)
+        
+        while intro: 
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    intro = False
+                    self.running = False
+                mouse_pos = pygame.mouse.get_pos()
+                mouse_pressed = pygame.mouse.get_pressed()
+            
+                if play_button.is_pressed(mouse_pos, mouse_pressed):
+                    intro = False
+                    
+                self.screen.blit(self.intro_background, (0, 0))
+                self.screen.blit(title, title_rect)
+                self.screen.blit(play_button.image, play_button.rect)
+                self.clock.tick(FPS)
+                pygame.display.update()
 
 g = Game()
 g.intro_screen()
